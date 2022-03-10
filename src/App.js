d69a511e3bf01afea5a3758960764b1f9cd99da0
forrestmorrison <forrestmorrison@gmail.com>
2022-03-10 10:59:20 -0600

    created App Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchForm from "./SearchForm";
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      articlesArray: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.punkapi.com/v2/beers")
+      .then( res => {
+          const articlesArray = res.data
+          this.setState({ articlesArray })
+      })
+  }
+
+  
+
+  render() {
+    return (
+      <ol>{this.state.articlesArray.map((article, index) => {
+        return (
+          <SearchForm />
+        )
+      })}</ol>
+    )
+  }
+
 }
+
 
 export default App;
