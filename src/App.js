8d888a8cd8d828bb190e2ae1ded3f9f581aa13d3
taplinseth <taplinseth@gmail.com>
2022-03-10 13:02:42 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import logo from './logo.svg';
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
+import React, { Component } from 'react';
 import './App.css';
-import Articlescard from './components/ArticlesCard'
 
-function App() {
-  return (
-    <div className="App">
-      <body className="App-header">
-        <ArticlesCard />
-      </body>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      articlesArray: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://hn.algolia.com/api/v1/search?query=Javascript")
+      .then(response => response.json())
+      .then(data => this.setState({articlesArray: data.hits}))
+      .catch(err => console.error(err))
+  }
+
+  
+
+  render() {
+    return (
+          <h3>News Articles</h3>
+          // <ol>{this.state.articlesArray}</ol>
+    )
+  }
+
 }
+
 
 export default App;
